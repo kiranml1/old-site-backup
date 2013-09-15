1cef85fcf9096a7f383e94be96ffd5bbc0a06a36
kiran kumar <kirankumar.amruthaluri@gmail.com>
2013-09-15 23:11:05 +0530

    separated out angular build

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('kiranmeApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
