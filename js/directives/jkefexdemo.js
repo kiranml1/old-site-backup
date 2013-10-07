7979a7bc705e004b81bbd28ebec2f99730a5caf6
kiran kumar <kirankumar.amruthaluri@gmail.com>
2013-10-08 02:24:43 +0530

    Added demos and blocks links

--- js/directives/jkefexdemo.js
+++ js/directives/jkefexdemo.js
@@ -6,7 +6,7 @@ jkefex.directive('jkefexdemo',function(){
 		},
 		replace:true,
 		template:'<div class="4u" ng-repeat="con in cont"><article class="box box-style2"><a href="{{con.tempurl}}" class="various image image-full"><img ng-src="{{con.thumb}}" alt="" /></a>'+
-					'<h3><a href="{{con.tempurl}}" style="text-decoration:none" class="various">'+
+					'<h3><a href="{{con.codeurl}}" style="text-decoration:none" class="various">'+
 					'{{con.title}}</a></h3>'+
 					'<p>{{con.subtitle}}</p></article></div>'
 		};
