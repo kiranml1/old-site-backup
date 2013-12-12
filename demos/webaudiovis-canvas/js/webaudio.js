dfc8934d20506847eab58b564913c34079eb1235
kiranml1 <kirankumar.amruthaluri@gmail.com>
2013-12-13 01:38:37 +0530

    Updates

--- demos/webaudiovis-canvas/js/webaudio.js
+++ demos/webaudiovis-canvas/js/webaudio.js
@@ -1,114 +1,113 @@
-/*
- * Copyright 2013 Boris Smus. All Rights Reserved.
-
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-
-var WIDTH = 1024;
-var HEIGHT = 360;
-
-// Interesting parameters to tweak!
+var ctx; //audio context 
+var buf; //audio buffer 
+var fft; //fft audio node 
+var samples = 1024; 
 var SMOOTHING = 0.8;
-var FFT_SIZE = 2048;
+var setup = false; //indicate if audio is set up yet 
+var WIDTH = 1024;
+var HEIGHT = 460;
+ 
+ 
+//init the sound system 
+function init() { 
+    console.log("in init"); 
+    try { 
+        ctx = new webkitAudioContext();
+        $('#small').html('Loading...'); //is there a better API for this? 
+        setupCanvas(); 
+        loadFile(); 
+    } catch(e) { 
+        alert('you need webaudio support' + e); 
+    } 
+} 
+window.addEventListener('load',init,false); 
+ 
+//load the mp3 file 
+function loadFile() { 
+    var req = new XMLHttpRequest(); 
+    req.open("GET","assets/Dooriyan.mp3",true); 
+    //we can't use jquery because we need the arraybuffer type 
+    req.responseType = "arraybuffer";
+    req.onprogress = function(e){
+            if(e.lengthComputable)
+            {
+                var percent = e.loaded/e.total;
+                $('#small').html('Loading '+Math.round(percent*100)+'% Please Wait...');
+            }
+        }; 
+    req.onload = function() { 
+        //decode the loaded data 
+        ctx.decodeAudioData(req.response, function(buffer) { 
+            buf = buffer; 
+            play(); 
+        });
+        $('#small').html('Audio Visualization'); 
+    }; 
+    req.send(); 
+} 
 
-function VisualizerSample(url) {
-  this.analyser = context.createAnalyser();
+function play() { 
+    //create a source node from the buffer 
+    var src = ctx.createBufferSource();  
+    src.buffer = buf; 
+     
+    //create fft 
+    fft = ctx.createAnalyser(); 
+    fft.fftSize = samples; 
+    fft.smoothingTimeConstant = SMOOTHING;
+     
+    //connect them up into a chain 
+    src.connect(fft); 
+    fft.connect(ctx.destination); 
+     
+    //play immediately 
+    src.noteOn(0); 
+    setup = true; 
+} 
 
-  this.analyser.connect(context.destination);
-  this.analyser.minDecibels = -140;
-  this.analyser.maxDecibels = 0;
-  loadSounds(this, {
-    buffer: 'Violan.mp3'
-    //buffer: 'sound.wav'
-  });
-  this.freqs = new Uint8Array(this.analyser.frequencyBinCount);
-  this.times = new Uint8Array(this.analyser.frequencyBinCount);
+var gfx; 
+function setupCanvas() { 
+	var canvas = document.querySelector('canvas');
+	var drawContext = canvas.getContext('2d');
+	canvas.width = WIDTH;
+	canvas.height = HEIGHT;
+    gfx = canvas.getContext('2d'); 
+    webkitRequestAnimationFrame(update); 
+} 
 
-  this.isPlaying = false;
-  this.startTime = 0;
-  this.startOffset = 0;
-}
+function update() { 
+    webkitRequestAnimationFrame(update); 
+    if(!setup) return; 
+    gfx.clearRect(0,0,WIDTH,HEIGHT); 
+     
+    var data = new Uint8Array(samples); 
+    fft.getByteFrequencyData(data); 
+    // gfx.fillStyle = 'red'; 
+    // for(var i=0; i<data.length; i++) { 
+    //     gfx.fillRect(100+i*4,100+256-data[i]*2,3,100); 
+    // } 
 
-// Toggle playback
-VisualizerSample.prototype.togglePlayback = function() {
-  if (this.isPlaying) {
-    // Stop playback
-    this.source.noteOff(0);
-    this.startOffset += context.currentTime - this.startTime;
-    console.log('paused at', this.startOffset);
-    // Save the position of the play head.
-  } else {
-    this.startTime = context.currentTime;
-    console.log('started at', this.startOffset);
-    this.source = context.createBufferSource();
-    // Connect graph
-    this.source.connect(this.analyser);
-    this.source.buffer = this.buffer;
-    this.source.loop = true;
-    // Start playback, but make sure we stay in bound of the buffer.
-    this.source.start(0, this.startOffset % this.buffer.duration);
-    // Start visualizer.
-    requestAnimFrame(this.draw.bind(this));
-  }
-  this.isPlaying = !this.isPlaying;
-}
-
-
-VisualizerSample.prototype.draw = function() {
-  this.analyser.smoothingTimeConstant = SMOOTHING;
-  this.analyser.fftSize = FFT_SIZE;
-
-  // Get the frequency data from the currently playing music
-  this.analyser.getByteFrequencyData(this.freqs);
-  this.analyser.getByteTimeDomainData(this.times);
-
-  var width = Math.floor(1/this.freqs.length, 10);
-
-  var canvas = document.querySelector('canvas');
-  var drawContext = canvas.getContext('2d');
-  canvas.width = WIDTH;
-  canvas.height = HEIGHT;
-  // Draw the frequency domain chart.
-  for (var i = 0; i < this.analyser.frequencyBinCount; i++) {
-    var value = this.freqs[i];
+// Draw the frequency domain chart.
+  for (var i = 0; i < fft.frequencyBinCount; i++) {
+    var value = data[i];
     var percent = value / 256;
     var height = HEIGHT * percent;
     var offset = HEIGHT - height - 1;
-    var barWidth = WIDTH/this.analyser.frequencyBinCount;
-    var hue = i/this.analyser.frequencyBinCount * 260;
-    drawContext.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
-    drawContext.fillRect(i * barWidth, offset, barWidth, height);
+    var barWidth = WIDTH/fft.frequencyBinCount;
+    var hue = i/fft.frequencyBinCount * 160;
+    gfx.fillStyle = 'hsl(' + hue + ', 100%, 50%)';
+    gfx.fillRect(i * barWidth, offset, barWidth, height);
   }
 
   // Draw the time domain chart.
-  for (var i = 0; i < this.analyser.frequencyBinCount; i++) {
-    var value = this.times[i];
+  for (var i = 0; i < fft.frequencyBinCount; i++) {
+    var value = data[i];
     var percent = value / 256;
     var height = HEIGHT * percent;
     var offset = HEIGHT - height - 1;
-    var barWidth = WIDTH/this.analyser.frequencyBinCount;
-    drawContext.fillStyle = 'black';
-    drawContext.fillRect(i * barWidth, offset, 1, 2);
+    var barWidth = WIDTH/fft.frequencyBinCount;
+    gfx.fillStyle = 'black';
+    gfx.fillRect(i * barWidth, offset, 1, 2);
   }
-
-  if (this.isPlaying) {
-    requestAnimFrame(this.draw.bind(this));
-  }
-}
-
-VisualizerSample.prototype.getFrequencyValue = function(freq) {
-  var nyquist = context.sampleRate/2;
-  var index = Math.round(freq/nyquist * this.freqs.length);
-  return this.freqs[index];
+     
 }
