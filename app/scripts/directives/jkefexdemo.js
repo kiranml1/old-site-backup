5a8919ece1d6d816ac6f1d56fec83512d4a2cdf7
kiranml1 <kirankumar.amruthaluri@gmail.com>
2014-01-01 20:19:18 +0530

    added image svg line painter

--- app/scripts/directives/jkefexdemo.js
+++ app/scripts/directives/jkefexdemo.js
@@ -5,9 +5,9 @@ jkefex.directive('jkefexdemo',function(){
 			cont:'='
 		},
 		replace:true,
-		template:'<div class="3u" ng-repeat="con in cont"><article class="box box-style2"><a href="{{con.tempurl}}" class="various image image-full"><img ng-src="{{con.thumb}}" alt="" /></a>'+
-					'<h4><a href="{{con.codeurl}}" style="text-decoration:none" class="various">'+
-					'{{con.title}}</a></h4>'+
-					'<small>{{con.subtitle}}</small></article></div>'
+		template:'<div class="2u" ng-repeat="con in cont"><article class="box box-style2"><a href="{{con.tempurl}}" class="various image image-full"><img ng-src="{{con.thumb}}" alt="" /></a>'+
+					'<h6 style="font-size:0.8em;"><a href="{{con.codeurl}}" style="text-decoration:none" class="various">'+
+					'{{con.title}}</a></h6>'+
+					'</article></div>'
 		};
 });
